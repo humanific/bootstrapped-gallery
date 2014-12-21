96fb17bb89911834e4afdc382469881a5e85d791
humanific <francois@humanific.com>
2014-12-21 14:11:02 +0700

    v0.01

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package bootstrapped_gallery
- * @version 0.1
+ * @version 0.01
  */
 /*
 Plugin Name: Bootstrapped gallery
 Plugin URI: https://github.com/humanific/bootstrapped-gallery
 Description: Shortcode for displaying bootstrap blueimp-gallery galleries
 Author: Francois Richir
-Version: 0.1
+Version: 0.01
 Author URI: http://humanific.com
 */
 
@@ -19,11 +19,7 @@ function bootstrapped_gallery_shortcode($atts) {
    global $post, $bootstrapped_galleries_num;
    if(!$bootstrapped_galleries_num) $bootstrapped_galleries_num = 0;
    $bootstrapped_galleries_num++;
-   wp_enqueue_style( 'blueimpgallery2', plugins_url( '/blueimp-gallery.min.css' , __FILE__ ));
-   wp_enqueue_style( 'blueimpgallery', plugins_url( '/bootstrap-image-gallery.min.css', __FILE__ ));
-   wp_enqueue_script( 'blueimpgallery1', plugins_url( '/jquery.blueimp-gallery.min.js' , __FILE__ ),array('bootstrap'));
-   wp_enqueue_script( 'blueimpgallery', plugins_url( '/bootstrap-image-gallery.min.js' , __FILE__ ),array('blueimpgallery1'));
-  
+
   
    if(isset($atts['ids'])){
       $pids = explode(',', $atts['ids']);
@@ -46,76 +42,79 @@ function bootstrapped_gallery_shortcode($atts) {
 
   ob_start();
   if ($images) :?>
-    <div class="gallery" id="bootstrapped-gallery-<?php echo $bootstrapped_galleries_num; ?>">
+    <div class="gallery" id="gallery<?php echo $bootstrapped_galleries_num; ?>">
     <div class="row">
       <?php foreach( $images as $k => $imagePost ): 
+        
         $image_attributes = wp_get_attachment_image_src(  $imagePost->ID, 'thumbnail' ); 
         $large = wp_get_attachment_image_src(  $imagePost->ID, 'large' ); 
+        $url = $atts['lightbox'] != 'false' ? $large[0] : get_attachment_link(  $imagePost->ID );
         ?><div class="<?php echo $span ;?>">
-        <a href="<?php echo $large[0]; ?>" data-toggle="tooltip" data-placement="top" class="thumbnail" data-gallery="bootstrapped-gallery-<?php echo $bootstrapped_galleries_num; ?>" title="<?php echo $imagePost->post_excerpt ?>" data-description="<?php echo $imagePost->post_content ?>">
+        <a href="<?php echo $url; ?>" data-toggle="tooltip<?php echo $bootstrapped_galleries_num; ?>" data-placement="top" class="thumbnail" data-gallery="bootstrapped-gallery-<?php echo $bootstrapped_galleries_num; ?>" data-title="<?php echo $imagePost->post_excerpt ?>"  title="<?php echo $imagePost->post_excerpt ?>" >
         <img src="<?php echo $image_attributes[0]; ?>" class="fullwidth"/>
         </a>
         </div>
       <?php endforeach ;?>
       </div>
     </div>
-    <?php if($bootstrapped_galleries_num==1):?>
-<!-- The Bootstrap Image Gallery lightbox, should be a child element of the document body -->
-<div id="blueimp-gallery" class="blueimp-gallery">
-    <!-- The container for the modal slides -->
+<?php if($bootstrapped_galleries_num==1):?>
+<div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls">
     <div class="slides"></div>
-    <!-- Controls for the borderless lightbox -->
     <h3 class="title"></h3>
+    <p class="description"></p>
     <a class="prev">‹</a>
     <a class="next">›</a>
     <a class="close">×</a>
     <a class="play-pause"></a>
     <ol class="indicator"></ol>
-    <!-- The modal dialog, which will be used to wrap the lightbox content -->
-    <div class="modal fade">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title"></h4>
-                </div>
-                <div class="modal-body next"></div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default pull-left prev">
-                        <i class="glyphicon glyphicon-chevron-left"></i>
-                        Previous
-                    </button>
-                    <button type="button" class="btn btn-primary next">
-                        Next
-                        <i class="glyphicon glyphicon-chevron-right"></i>
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
 </div>
 
-
-<script type="text/jscript">
-
- jQuery(function ($) {
-  //$('[data-toggle="tooltip"]').tooltip();
-  var borderless = true;
-  $('#blueimp-gallery').data('useBootstrapModal', !borderless);
-  $('#blueimp-gallery').toggleClass('blueimp-gallery-controls', borderless);
+<?php endif;?> 
+<script>
+ jQuery(document).ready(function ($) {
+  <?php if($atts['tooltip'] == 'true') :?> $('[data-toggle="tooltip<?php echo $bootstrapped_galleries_num; ?>"]').tooltip(); <?php endif;?> 
+  <?php if($atts['lightbox'] != 'false') :?> 
+  $('#gallery<?php echo $bootstrapped_galleries_num; ?>').click(function (event) {
+  event = event || window.event;
+    var target = event.target || event.srcElement,
+        link = target.src ? target.parentNode : target;
+    blueimp.Gallery($(this).find('a'), {
+      index: link, 
+      event: event, 
+      titleProperty: 'data-title',
+      onslide: function (index, slide) {
+        $(this.container).find('.title').html($(this.list[index]).attr('data-title') +'<br><small>'+$(this.list[index]).attr('data-description')+'</small>');
+        }
+    });
+    return false;
+  });
+  <?php endif ;?>
 })
 </script>
-<?php endif ;?>
+
 <?php 
 add_action( 'wp_print_styles', 'add_gallery_styles', 100 );
-
-
 endif; 
 return ob_get_clean();
 }
 
 
-remove_shortcode('gallery', 'gallery_shortcode'); // removes the original shortcode
-add_shortcode('gallery', 'bootstrapped_gallery_shortcode'); // add your own shortcode
+remove_shortcode('gallery', 'gallery_shortcode');
+add_shortcode('gallery', 'bootstrapped_gallery_shortcode');
 
+
+
+function bootstrapped_gallery_scripts() {
+   wp_enqueue_script( 'blueimp-gallery', plugins_url( '/js/blueimp-gallery.min.js' , __FILE__ ),array('jquery'));
+}
+
+
+add_action( 'wp_enqueue_scripts', 'bootstrapped_gallery_scripts' );
+
+function bootstrapped_gallery_styles() {
+  wp_enqueue_style( 'blueimp-gallery', plugins_url( '/css/blueimp-gallery.css' , __FILE__ ));
+}
+
+
+add_action( 'wp_print_styles', 'bootstrapped_gallery_styles', 100 );
 ?>
